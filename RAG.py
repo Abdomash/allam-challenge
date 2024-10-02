2c5dc106c23c5d92ac987d92be2e0061edd4aae9
Abdomash <abdomash2010@gmail.com>
2024-10-02 08:48:35 -0500

    Improve RAG

--- RAG.py
+++ RAG.py
@@ -2,34 +2,50 @@ import json
 import random
 
 class RAG:
-    def __init__(self, filepath, qafiya):
-        self.message = "RAG initialized"
+    def __init__(self, filepath="qawafi-database.json", qafiya=None):
         self.qafiya = qafiya
+        self.message = None
         self.db = None
         with open(filepath, 'r', encoding="utf-8") as f:
             self.db = json.load(f)["data"]
-        self.update(None, qafiya)
+        self.setQafiya(qafiya)
     
     def wrap(self, prompt, previous_shatrs=None, feedback=None):
-        return f'''
-        اكتب شطر واحد لجزء من القصيدة. 
-        قافية القصيدة هي "{self.qafiya}".
-        هنا بعض الامثلة لكلمات تنتهي بهذه القافية
-        يمكن ان تستعمل هذه الكلمات او كلمات اخرى بنفس القافية:
-        <QafiyaExamples>{random.sample(self.message, 10)}</QafiyaExamples>
-        هنا الطلب اللي وضعه المستخدم:
-        <prompt>{prompt}</prompt>
-        هنا الشطور السابقة التي تم ادخالها بنجاح:        
-        <previous-successful-outputs>{previous_shatrs}</previous-successful-outputs>
-        وهنا بعض النصائح على هذا اخر شطر تم ادخاله:
-        <feedback>{feedback}</feedback>
-        '''
+        full_text = ""
+        full_text += "اكتب شطر واحد لجزء من قصيدة.\n"
+        
+        if self.qafiya:
+            full_text += f" قافية القصيدة هي '{self.qafiya}'. "
+
+        if self.message:
+            full_text += "هنا بعض الامثلة لكلمات تنتهي بهذه القافية: \n"
+            # full_text += "<QafiyaExamples>"
+            full_text += ", ".join(random.sample(self.message, 10))
+            full_text += "\n"
+            # full_text += "</QafiyaExamples>"
+        
+        if prompt:
+            full_text += f"هنا الطلب اللي وضعه المستخدم:\n"
+            full_text += f"{prompt}\n"
+        
+        if previous_shatrs:
+            full_text += "هنا الشطور السابقة:\n"
+            full_text += f"{previous_shatrs}\n"
+
+        if feedback:
+            full_text += "هنا بعض النصائح على هذا اخر شطر تم ادخاله:\n"
+            full_text += f"{feedback}\n"
+
+        return full_text
     
-    def update(self, wazn, qafiya):
+    def update(self, qafiya):
         self.message = self.setQafiya(qafiya)
         self.qafiya = qafiya
 
     def setQafiya(self, qafiya):
+        if not qafiya:
+            return None
+
         def processed(word):
             if word.endswith(qafiya):
                 return word
