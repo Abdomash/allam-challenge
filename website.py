5ea19b11b2e5fa3db29c4d42bbb12ed902bb0394
Abdomash <abdomash2010@gmail.com>
2024-10-14 01:42:36 -0500

    Improve website UI & and poet+bahr options

--- website.py
+++ website.py
@@ -5,6 +5,9 @@ from flask_session import Session
 from LLMInterface import FakeGenerator
 from main import generate_qasida
 
+from time import sleep
+
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'  # Use server-side session
@@ -13,7 +16,7 @@ Session(app)
 llm = FakeGenerator()
 shatr_generator = ShatrGenerator(llm)
 
-def process_prompt(prompt):
+def process_prompt(prompt, selected_bahr, selected_poet):
     qasida = generate_qasida(prompt, shatr_generator)
     return qasida
 
@@ -24,7 +27,8 @@ def chat():
 
     if request.method == 'POST':
         user_input = request.form['prompt']
-        output = process_prompt(user_input)
+        selected_bahr = request.form['bahr']
+        output = process_prompt(user_input, selected_bahr, None)
         # Append to the chat log
         session['chat_log'].append({'prompt': user_input, 'response': output})
     
