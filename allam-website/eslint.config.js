8678904518531ed623486aa42428f7fe37bb9ffe
Abdomash <abdomash2010@gmail.com>
2024-11-02 19:03:14 -0500

    Fix eslint & remove prior content

--- allam-website/eslint.config.js
+++ allam-website/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pluginTailwindCSS from 'eslint-plugin-tailwindcss'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      tailwindcss: pluginTailwindCSS,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...pluginTailwindCSS.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
