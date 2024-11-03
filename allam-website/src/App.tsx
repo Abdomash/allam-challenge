7819fb22a5cb6d8069c526c522cc6c5a114d029d
Abdomash <abdomash2010@gmail.com>
2024-11-02 20:16:23 -0500

    Use Amiri and Arabic format

--- allam-website/src/App.tsx
+++ allam-website/src/App.tsx
@@ -1,7 +1,9 @@
+import { LandingPage } from './pages/LandingPage'
+
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <LandingPage />
     </div>
   )
 }
---
+++ allam-website/src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+export function LandingPage() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="m-4 text-4xl">الشاعر علام</h1>
+    </div>
+  )
+}
